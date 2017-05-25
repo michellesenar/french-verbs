89c42d950bd37511ee49e535a0c2fbbe7cdd9677
Michelle Senar <senarmichelle@gmail.com>
2017-05-24 17:21:52 -0700

    Deployment stuff, new secret key, etc.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn frverbs.wsgi --log-file -
--- frverbs/settings.py
+++ frverbs/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=my+psp%d4&0*w8o5%&^=$l=_r@%6iaqu6@463ppt5%uc8$lyq'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0']
 
 
 # Application definition
@@ -81,6 +82,8 @@ DATABASES = {
     }
 }
 
+#db_from_env = dj_database_url.config()
+#DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
