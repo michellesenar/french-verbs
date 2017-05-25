cdae964e846b4e25441853928d7076786163a7be
Michelle Senar <senarmichelle@gmail.com>
2017-05-24 18:09:58 -0700

    Load environment variables

--- frverbs/settings.py
+++ frverbs/settings.py
@@ -12,9 +12,13 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 import dj_database_url
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -123,3 +127,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Extra places for collectstatic to find static files.
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
